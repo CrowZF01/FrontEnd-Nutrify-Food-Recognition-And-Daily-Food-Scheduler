a817a69bc7ec3fc7c228d95507c7e53afeca476f
71241153_Felix_Benedictus_Setiawan <123315666+CrowZF01@users.noreply.github.com>
2026-05-22 17:16:20 +0700

    Delete nutrify directory

--- nutrify/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-
-void main() {
-  testWidgets('NutriFy app smoke test', (WidgetTester tester) async {
-    // Smoke test placeholder
-    expect(1 + 1, equals(2));
-  });
-}
